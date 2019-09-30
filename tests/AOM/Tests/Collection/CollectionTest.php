5bee6c3b31c9e388e60bf1bce114da96e3ddb728
Normamarth Radu Dragomir <noramarth@aom.ro>
2019-09-30 22:23:30 +0000

    WIP: added test skeleton

---
+++ tests/AOM/Tests/Collection/CollectionTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace AOM\Tests\Collection;
+
+use AOM\Lib\Collection\Collection;
+use PHPUnit_Framework_TestCase;
+
+class CollectionTest extends PHPUnit_Framework_TestCase
+{
+
+    public function testRemoveElement()
+    {
+
+    }
+
+    public function testIsEmpty()
+    {
+
+    }
+
+    public function testUpdate()
+    {
+
+    }
+
+    public function testApplyToAll()
+    {
+
+    }
+
+    public function testSelectStackable()
+    {
+
+    }
+
+    public function testSet()
+    {
+
+    }
+
+    public function testFilter()
+    {
+
+    }
+
+    public function testInsert()
+    {
+
+    }
+
+    public function testMakeElementStackable()
+    {
+
+    }
+
+    public function testGetFirstElementByKey()
+    {
+
+    }
+
+    public function testGetElementByKey()
+    {
+
+    }
+
+    public function testContains()
+    {
+
+    }
+
+    public function testApplyToAllOfType()
+    {
+
+    }
+
+    public function testIsStackable()
+    {
+
+    }
+}
